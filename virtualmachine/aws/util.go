81503fb231b28062329f669e9ae14e554bc33636
alex <alex@criticalstack.com>
2017-01-04 13:43:01 -0500

    mutiple sg

--- virtualmachine/aws/util.go
+++ virtualmachine/aws/util.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/apcera/util/uuid"
@@ -192,9 +193,11 @@ func instanceInfo(vm *VM) *ec2.RunInstancesInput {
 	}
 
 	var sgid []*string
-	if vm.SecurityGroup != "" {
-		sgid = make([]*string, 1)
-		sgid[0] = aws.String(vm.SecurityGroup)
+	if vm.SecurityGroups != "" {
+		sgid = make([]*string, 0)
+		for _, sg := range strings.Split(vm.SecurityGroups, ",") {
+			sgid = append(sgid, aws.String(sg))
+		}
 	}
 
 	var ud *string
